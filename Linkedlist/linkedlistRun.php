1a4ebfad761af0e919b1e19eb77f7c4040b0f866
Muhammad Raza <muhammad.raza@coeus-solutions.de>
2014-08-26 19:21:45 +0500

    Add Comments

--- Linkedlist/linkedlistRun.php
+++ Linkedlist/linkedlistRun.php
@@ -1,5 +1,15 @@
 <?php
-
+/**
+ * Contains a single function call
+ *
+ * This file contains a single function call to linkedlistrun function in 
+ * linkedlistDriver Class
+ *
+ * @category Training/Learning PHP
+ * @package Learning Test Driven Development
+ * @version v 1.0
+ */
 require_once 'linkedlistDriver.php';
 
-linkedlistrun();
+$linkedlistDriverObj=new LinkedlistDriver();
+$linkedlistDriverObj->linkedlistrun();
